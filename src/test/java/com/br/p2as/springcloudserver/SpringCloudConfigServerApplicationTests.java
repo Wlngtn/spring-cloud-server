62d804ea42996f8e5092fe918de18da19b608867
Wellington <wellington.vasconcelos@gmail.com>
2022-07-11 20:28:21 -0300

    melhorais

---
+++ src/test/java/com/br/p2as/springcloudserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.p2as.springcloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
